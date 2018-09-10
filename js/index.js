dd5c8317572c409262552ed869d30d109b40a5cc
Grzegorz <kalarus.g@gmail.com>
2018-09-10 17:28:37 +0200

    animation

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var $bannerBtn = $('.btn-banner');
     var $bannerTxt = $('.about__banner--content').find('p');
     var $mission = $('.mission');
     var $vision = $('.vision');
 
-    
 
-    $bannerBtn.on('click', function() {
+
+    $bannerBtn.on('click', function () {
         $mission.toggleClass('strong');
         $vision.toggleClass('strong');
-        if($vision.hasClass('strong')) {
-            $bannerTxt.fadeOut(300, function() {
-                $(this).text('Do nisi sint elit adipisicing velit amet incididunt sit ad. Aliqua sit sint commodo nulla. Tempor aliquip proident sunt officia quis minim deserunt consequat laborum. Pariatur reprehenderit duis ex deserunt sint laborum duis labore labore ullamco nisi incididunt. Amet sit consequat ipsum aliquip dolore et consequat cupidatat aliqua eu ut ut ut quis. Mollit pariatur est veniam dolor. Minim in ullamco consectetur pariatur sit ea cillum dolore. Amet do mollit deserunt quis.').fadeIn(300);
+        if ($vision.hasClass('strong')) {
+            $bannerTxt.fadeOut(250, function () {
+                $(this).text('Do nisi sint elit adipisicing velit amet incididunt sit ad. Aliqua sit sint commodo nulla. Tempor aliquip proident sunt officia quis minim deserunt consequat laborum. Pariatur reprehenderit duis ex deserunt sint laborum duis labore labore ullamco nisi incididunt. Amet sit consequat ipsum aliquip dolore et consequat cupidatat aliqua eu ut ut ut quis. Mollit pariatur est veniam dolor. Minim in ullamco consectetur pariatur sit ea cillum dolore. Amet do mollit deserunt quis.').fadeIn(250);
             })
         } else {
-            
-            $bannerTxt.fadeOut(300, function() {
-                $(this).text('Wierzymy, że kreatywność i postęp nie mają granic, a wychodzenie pozaramy otwiera wiele możliwości, dlatego tworzymy eventy, w których podstawą jest dostarczanie niezapomnianych emocji. W naszych produkcjach dążymy do kreowania zaskakującej i porywającej przestrzeni, w której uczestnicy eventu mogą poczuć się wyjątkowo i niepowatrzalnie. Multidyscyplinarny zespół, odwaga i nastawienie na sukces to fundamenty naszych działań.').fadeIn(300);
+            $bannerTxt.fadeOut(250, function () {
+                $(this).text('Wierzymy, że kreatywność i postęp nie mają granic, a wychodzenie pozaramy otwiera wiele możliwości, dlatego tworzymy eventy, w których podstawą jest dostarczanie niezapomnianych emocji. W naszych produkcjach dążymy do kreowania zaskakującej i porywającej przestrzeni, w której uczestnicy eventu mogą poczuć się wyjątkowo i niepowatrzalnie. Multidyscyplinarny zespół, odwaga i nastawienie na sukces to fundamenty naszych działań.').fadeIn(250);
             });
         }
     })
-     
+
 })
